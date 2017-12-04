b79ccf1f4a653ea87dfc3cc5f65f138e61cbc85a
Abhinav Bhatia <bhatiaabhinav93@gmail.com>
2017-12-04 15:12:12 +0800

    ers scripts: render off by default

--- scripts/ers_eval.bat
+++ scripts/ers_eval.bat
@@ -1,1 +1,2 @@
-"%GYM_PYTHON%" -m baselines.a2c.run_general --env=%1 --render=True --no_training=True --num_cpu=8 --saved_model="%OPENAI_LOGDIR%\%1\%2\model" %3 %4 %5 %6 %7 %8 %9
+taskkill /f /im "ERSEnv*"
+"%GYM_PYTHON%" -m baselines.a2c.run_general --env=%1 --no_training=True --num_cpu=8 --saved_model="%OPENAI_LOGDIR%\%1\%2\model" %3 %4 %5 %6 %7 %8 %9
--- scripts/ers_train.bat
+++ scripts/ers_train.bat
@@ -1,1 +1,2 @@
-"%GYM_PYTHON%" -m baselines.a2c.run_general --env=%1 --render=True --num_cpu=8 %2 %3 %4 %5 %6 %7 %8 %9
+taskkill /f /im "ERSEnv*"
+"%GYM_PYTHON%" -m baselines.a2c.run_general --env=%1 --num_cpu=8 %2 %3 %4 %5 %6 %7 %8 %9
