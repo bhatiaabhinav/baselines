6224adfedc5842729030a165c37bd7b8ec7b450a
Abhinav Bhatia <bhatiaabhinav93@gmail.com>
2018-08-09 03:28:37 +0800

    tensorboard utility script

---
+++ scripts/tb_canonical.sh
@@ -0,0 +1,9 @@
+cd $OPENAI_LOGDIR
+$ACTIVATE_GYM_PYTHON
+tensorboard --logdir=v6:pyERSEnv-ca-dynamic-30-v6,\
+blipsv6:pyERSEnv-ca-dynamic-blips-30-v6,\
+sgv6:SgERSEnv-ca-dynamic-30-v6,\
+sgblipsv6:SgERSEnv-ca-dynamic-blips-30-v6,\
+bssv0:BSSEnv-v0,\
+bssv1:BSSEnv-v1,\
+bssv2:BSSEnv-v2
