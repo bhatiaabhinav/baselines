acdadd3e19fec0752cffd8dd4b87ba30d4c301b5
Abhinav Bhatia <bhatiaabhinav93@gmail.com>
2018-08-10 02:54:48 +0800

    changed canonocal cap env from cap5 to cap8

--- scripts/tb_canonical.sh
+++ scripts/tb_canonical.sh
@@ -5,10 +5,10 @@ v6:pyERSEnv-ca-dynamic-30-v6,\
 blipsv6:pyERSEnv-ca-dynamic-blips-30-v6,\
 sgv6:SgERSEnv-ca-dynamic-30-v6,\
 sgblipsv6:SgERSEnv-ca-dynamic-blips-30-v6,\
-capv6:pyERSEnv-ca-dynamic-cap5-30-v6,\
-blipscapv6:pyERSEnv-ca-dynamic-blips-cap5-30-v6,\
-sgcapv6:SgERSEnv-ca-dynamic-cap5-30-v6,\
-sgblipscapv6:SgERSEnv-ca-dynamic-blips-cap5-30-v6,\
+capv6:pyERSEnv-ca-dynamic-cap8-30-v6,\
+blipscapv6:pyERSEnv-ca-dynamic-blips-cap8-30-v6,\
+sgcapv6:SgERSEnv-ca-dynamic-cap8-30-v6,\
+sgblipscapv6:SgERSEnv-ca-dynamic-blips-cap8-30-v6,\
 bssv0:BSSEnv-v0,\
 bssv1:BSSEnv-v1,\
 bssv2:BSSEnv-v2\
